23bcdc1a0b204f792c06b3e41169c218b6226325
Serhii Shtokal <serhiishtokal@gmail.com>
2021-03-11 02:31:14 +0100

    offset and limit bugfix & write tracks to file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import {getTracksFromPlaylist,getLikedTracks} from "./spotifyTracksMethods.js";
+import fs from "fs"
 
 
-getTracksFromPlaylist("7cTTl6rO4vpCh9IzUmi7IX").then((tracks)=>{console.log( tracks,tracks.length)})
+// getTracksFromPlaylist("7cTTl6rO4vpCh9IzUmi7IX",100).then((tracks)=>{
+//     storeData(tracks,"tracks.json")
+// })
 
-//getLikedTracks(100).then((tracks)=>{console.log(tracks.length, tracks)})
+const storeData = (data, path) => {
+    try {
+        fs.writeFileSync(path, JSON.stringify(data))
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+getLikedTracks(400).then((tracks)=>{
+    console.log(tracks.length,tracks[0])
+    storeData(tracks,"tracks.json")
+})
--- src/spotifyTracksMethods.js
+++ src/spotifyTracksMethods.js
@@ -1,29 +1,31 @@
 import {requestToApi} from "./request.js"
-import {MAX_LIKED_TRACKS_LIMIT, MAX_PLAYLIST_TRACKS_LIMIT} from "./conf.js";
+import {MAX_LIKED_TRACKS_LIMIT, MAX_PLAYLIST_TRACKS_LIMIT, MIN_TRACKS_LIMIT} from "./conf.js";
 
-let getTracksFromPlaylist= async (playlistId,offset=0,trackLimit="")=> {
-    let limit=prepareTrackLimit(trackLimit,MAX_PLAYLIST_TRACKS_LIMIT)
-    let url="https://api.spotify.com/v1/playlists/"+playlistId+"/tracks?offset=0&"+limit+"&additional_types=track%2Cepisode&market=from_token"
+let getTracksFromPlaylist= async (playlistId,offset=0,trackLimit=-1)=> {
+    let url = new URL('https://api.spotify.com/v1/playlists/'+playlistId+'/tracks?additional_types=track%2Cepisode&market=from_token');
+    appendNumberParameter(url.searchParams,"limit",trackLimit,MAX_PLAYLIST_TRACKS_LIMIT,MIN_TRACKS_LIMIT)
+    appendNumberParameter(url.searchParams,"offset",offset,undefined,0)
+
     let method= "GET"
     let response= await requestToApi(url,method)
     return response.items
 }
 
-let getLikedTracks = async (offset=0,trackLimit="")=>{
-    let limit=prepareTrackLimit(trackLimit,MAX_LIKED_TRACKS_LIMIT)
-    let url="https://api.spotify.com/v1/me/tracks?"+limit+"&offset=0&market=from_token"
+let getLikedTracks = async (offset=0,trackLimit=-1)=>{
+    let url = new URL('https://api.spotify.com/v1/me/tracks?market=from_token');
+    appendNumberParameter(url.searchParams,"limit",trackLimit,MAX_LIKED_TRACKS_LIMIT,MIN_TRACKS_LIMIT)
+    appendNumberParameter(url.searchParams,"offset",offset,undefined, 0)
+
     let method= "GET"
     let response= await requestToApi(url,method)
     return response.items
 }
 
-let prepareTrackLimit=(responseTrackLimit, maxLimit) =>{
-    let trackLimit=""
-    if (responseTrackLimit<=maxLimit &&responseTrackLimit>=1) trackLimit="limit="+responseTrackLimit
-    return trackLimit
+let appendNumberParameter=(params, paramName,value, maxValue=Number.MAX_SAFE_INTEGER, minValue=Number.MIN_SAFE_INTEGER) =>{
+    if(value<=maxValue &&value>=minValue)
+    params.append(paramName, value)
+    else params.append(paramName, maxValue)
 }
-
-
 
 
 export {getTracksFromPlaylist,getLikedTracks}
